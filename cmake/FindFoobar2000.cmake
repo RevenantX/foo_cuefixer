ec8dfe88aa56439e23992f06d9a0f6171449a015
foxx1337 <foxx1337@yahoo.com>
2023-02-12 23:25:54 +0100

    add library handling to CMake for foobar2000

--- cmake/FindFoobar2000.cmake
+++ cmake/FindFoobar2000.cmake
@@ -12,7 +12,7 @@
 #  FOOBAR2000_LIBS, containing debug and release for all of
 #    FOOBAR2000_SDK_<DEBUG|RELEASE>_LIB
 #    FOOBAR2000_COMPONENT_CLIENT_<DEBUG|RELEASE>_LIB
-#    SHARED-${uplatform}_<DEBUG|RELEASE>_LIB
+#    SHARED-<Win32|x64|ARM64>_<DEBUG|RELEASE>_LIB
 #    PFC_<DEBUG|RELEASE>_LIB
 
 find_path(
@@ -52,6 +52,7 @@ function(combine_debug_optimized base_name)
     find_foobar2000_lib(${base_name} Debug)
     find_foobar2000_lib(${base_name} Release)
     set(${base_uname}_LIBS debug ${${base_uname}_DEBUG_LIB} optimized ${${base_uname}_RELEASE_LIB} PARENT_SCOPE)
+    mark_as_advanced(${base_uname}_LIBS ${base_uname}_DEBUG_LIB ${base_uname}_RELEASE_LIB)
 endfunction()
 
 combine_debug_optimized(foobar2000_SDK)
@@ -71,7 +72,38 @@ find_package_handle_standard_args(
     FOOBAR2000_PFC_INCLUDE_DIR
 )
 
+function(create_foobar2000_namespace_lib base_name include_dir)
+    if(NOT TARGET foobar2000::${base_name})
+        string(TOUPPER "${base_name}" base_uname)
+        add_library(foobar2000::${base_name} UNKNOWN IMPORTED)
+        set_target_properties(
+            foobar2000::${base_name} PROPERTIES
+                IMPORTED_LOCATION ${${base_uname}_RELEASE_LIB}
+                IMPORTED_LOCATION_DEBUG ${${base_uname}_DEBUG_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${include_dir}
+        )
+    endif()
+endfunction()
+
 if(FOOBAR2000_FOUND)
     set(FOOBAR2000_INCLUDE_DIRS ${FOOBAR2000_SDK_INCLUDE_DIR} ${FOOBAR2000_PFC_INCLUDE_DIR})
     set(FOOBAR2000_LIBS ${FOOBAR2000_SDK_LIBS} ${FOOBAR2000_COMPONENT_CLIENT_LIBS} ${SHARED-${uplatform}_LIBS} ${PFC_LIBS})
+
+    create_foobar2000_namespace_lib(foobar2000_SDK ${FOOBAR2000_SDK_INCLUDE_DIR})
+    create_foobar2000_namespace_lib(foobar2000_component_client ${FOOBAR2000_SDK_INCLUDE_DIR})
+    create_foobar2000_namespace_lib(shared-${platform} ${FOOBAR2000_SDK_INCLUDE_DIR})
+    create_foobar2000_namespace_lib(pfc ${FOOBAR2000_PFC_INCLUDE_DIR})
+
+    if(NOT TARGET foobar2000::foobar2000)
+        add_library(foobar2000::foobar2000 INTERFACE IMPORTED)
+        set_property(
+            TARGET foobar2000::foobar2000 PROPERTY
+            INTERFACE_LINK_LIBRARIES
+                foobar2000::foobar2000_SDK
+                foobar2000::foobar2000_component_client
+                foobar2000::shared-${platform}
+                foobar2000::pfc
+        )
+        add_library(foobar2000 ALIAS foobar2000::foobar2000)
+    endif()
 endif()
